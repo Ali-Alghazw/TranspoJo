092a8379f9742e4c20f58ed93926a711956afb7c
Ali Alghazw <mackaiden03@gmail.com>
2025-05-07 13:39:01 +0300

    fixing the styling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,11 +13,6 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
     public IActionResult Pick()
     {
         return View();
